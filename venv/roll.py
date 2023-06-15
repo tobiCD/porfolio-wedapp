dfd408f72bdb4f385957030220f53fa913f4aa8f
Chu Đăng Khôi <106723115+tobiCD@users.noreply.github.com>
2023-06-15 21:58:54 +0700

    roll.py

--- venv/roll.py
+++ venv/roll.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import streamlit as st
 st.set_page_config(layout="wide")
-
-df=pd.read_csv("data2.csv",sep=",")
-col1 , col2 , col3=st.columns([25,25,25])
 uploaded_file = st.file_uploader(
     "Choose your database", accept_multiple_files=False)
 if uploaded_file is not None:
     file_name = uploaded_file
 else:
     file_name = "data2.csv"
+df=pd.read_csv(file_name,sep=",")
+col1 , col2 , col3=st.columns([25,25,25])
+
 with col3 :
     for index , row in df[:2].iterrows():
         st.header(row["title"])
